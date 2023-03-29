7238886e9f611546353a8569fae54e6a4cc67a82
Davi Belo <davibelocontato@gmail.com>
2023-03-29 12:55:33 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+//https://jsonplaceholder.typicode.com/posts
+
+async function readPost(){
+    let postArea = document.querySelector('.posts');
+    postArea.innerHTML = 'Carregando...'
+
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let json = await response.json();
+
+    if(json.length > 0){
+        postArea.innerHTML = ''
+
+        for(let i in json){
+            let postHtml = `<div><h1>${json[i].title}</h1>${json[i].body}<hr></div>`
+            postArea.innerHTML += postHtml
+        }
+
+    } else{
+        postArea.innerHTML = 'Nao há'
+    }
+}
+
+readPost()
